a07fba5fcff7af41b229b0d2bb3b9a369cda05c7
testusage <150278008+testusage@users.noreply.github.com>
2023-11-09 08:19:28 +0530

    Update doctor.cs

--- doctor.cs
+++ doctor.cs
@@ -1,6 +1,58 @@
 using System;
 using System.Data.SqlClient;
 
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create conn
+            String connString = "Data Source=DESKTOP-NVND1RL\\SQLEXPRESS;Initial Catalog=University;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connString);
+
+            //Open conn
+            conn.Open();
+
+            //Insert
+            String query = "insert into doctor (DoctorID , DoctorName , Department , Fees) Values(@DoctorID , @DoctorName , @Department , @Fees)";
+            SqlCommand command1 = new SqlCommand(query, conn);
+
+            command1.Parameters.Add("@DoctorId",16);
+            command1.Parameters.Add("@DoctorName", "D");
+            command1.Parameters.Add("@Department", "ECE");
+            command1.Parameters.Add("@Fees", 7000);
+
+            command1.ExecuteNonQuery();
+
+            //Select
+            SqlCommand cmd = new SqlCommand("select * from doctor",conn);
+
+            //Data Reader
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Console.WriteLine(dr[0]);
+                Console.WriteLine(dr[1]);
+                Console.WriteLine(dr[2]);
+                Console.WriteLine(dr[3]);
+                Console.WriteLine("");
+            }
+
+            //Close conn
+            conn.Close();
+
+            Console.ReadKey();
+            
+        }
+    }
+}
+
+-------------------------------------------------------
+
+using System;
+using System.Data.SqlClient;
+
 namespace ConsoleApp1
 {
     internal class dbconnectivity
